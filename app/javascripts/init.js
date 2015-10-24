7647a70cca0a14d4387d315d2ef496d45ce2f79d
Adam <adam@sportstechinc.com>
2015-10-24 19:42:11 -0400

    adds the BMI Calculator to the app

--- app/javascripts/init.js
+++ app/javascripts/init.js
@@ -4,39 +4,86 @@ import {makeDOMDriver, h} from '@cycle/dom';
 import {log} from './helpers'
 
 document.addEventListener("DOMContentLoaded", function() {
-  function messageForCount(count) {
-    if (count < 2) {
-      return "Low";
-    } else if (count < 5) {
-      return "Medium";
+
+  // BMI CALCULATOR
+  function bmiStatus(bmi) {
+    if (bmi < 18.5) {
+      return "Underweight"
+    } else if (bmi < 24.9) {
+      return "Healthy"
+    } else if (bmi < 29.9) {
+      return "Overweight"
     } else {
-      return "High";
+      return "Obese"
     }
   }
 
   function main({DOM}) {
-    const action$ = Cycle.Rx.Observable.merge(
-      DOM.select('.decrement').events('click').map(ev => -1),
-      DOM.select('.increment').events('click').map(ev => +1)
-    ).map(log);
+    let changeWeight$ = DOM.select('#weight').events('input').map(ev => ev.target.value);
+    let changeHeight$ = DOM.select('#height').events('input').map(ev => ev.target.value);
 
-    const count$ = action$
-      .startWith(0)
-      .scan((total, change) => total + change);
+    let bmi$ = Cycle.Rx.Observable.combineLatest(
+      changeWeight$.startWith(70),
+      changeHeight$.startWith(170),
+      (weight, height) => {
+        let heightMeters = height * 0.01;
+        let bmi = Math.round(weight / (heightMeters * heightMeters));
+        return {weight, height, bmi};
+      }
+    );
 
-    let requests = {
-      DOM: count$.map(count => 
+    return {
+      DOM: bmi$.map(({weight, height, bmi}) => 
         h('div', [
-          h('button.decrement', 'Decrement'),
-          h('button.increment', 'Increment'),
-          h('p', `Counter: ${count}`),
-          h('p', messageForCount(count))
+          h('div', [
+            `Weight: ${weight} kg`,
+            h('input#weight', {type:'range', min: 40, max: 140, value: weight}),
+          ]),
+          h('div', [
+            `Height: ${height} cm`,
+            h('input#height', {type:'range', min: 140, max: 210, value: height}),
+          ]),
+          h('h2', `BMI is ${bmi}`),
+          bmiStatus(bmi)
         ])
       )
-    };
-    return requests;
+    }
   }
 
+  // INCREMENT/DECREMENT
+  // function messageForCount(count) {
+  //   if (count < 2) {
+  //     return "Low";
+  //   } else if (count < 5) {
+  //     return "Medium";
+  //   } else {
+  //     return "High";
+  //   }
+  // }
+  // function main({DOM}) {
+  //   const action$ = Cycle.Rx.Observable.merge(
+  //     DOM.select('.decrement').events('click').map(ev => -1),
+  //     DOM.select('.increment').events('click').map(ev => +1)
+  //   ).map(log);
+
+  //   const count$ = action$
+  //     .startWith(0)
+  //     .scan((total, change) => total + change);
+
+  //   let requests = {
+  //     DOM: count$.map(count => 
+  //       h('div', [
+  //         h('button.decrement', 'Decrement'),
+  //         h('button.increment', 'Increment'),
+  //         h('p', `Counter: ${count}`),
+  //         h('p', messageForCount(count))
+  //       ])
+  //     )
+  //   };
+  //   return requests;
+  // }
+
+  // CHECKBOX
   // function log(thing) {
   //   console.log(thing);
   //   return thing;
