35bb814db4a6b7105ccdf9fca14b6af487a32e3b
ajbertra91 <ajbertra91@gmail.com>
2015-10-28 21:53:56 -0400

    adds a few more questions to the game.js

---
+++ app/javascripts/game.js
@@ -0,0 +1,96 @@
+export const game = {
+  game_title: "Bible Quiz",
+  questions: [
+    {
+      question_id: 1,
+      question: "Complete the following quote by Jesus: 'Whosoever will come after me, let him deny himself,…'",
+      choices: [
+        "deny his father and mother, and follow me.",
+        "and take up my commandments.",
+        "and love his brother.",
+        "and take up his cross, and follow me."
+      ],
+      answer: 4,
+      reference: "Mark 8:34",
+      link: "https://www.blueletterbible.org/kjv/mar/8/34"
+    },
+    {
+      question_id: 2,
+      question: "From which Old Testament book did Peter derive his initiative to replace Judas Iscariot?",
+      choices: [
+        "Genesis",
+        "Psalms",
+        "Isaiah",
+        "Joel"
+      ],
+      answer: 1,
+      reference: "Acts 1:20-22",
+      link: "https://www.blueletterbible.org/kjv/act/1/20"
+    },
+    {
+      question_id: 3,
+      question: "Paul tells the Thessalonians that he had previously suffered in what city?",
+      choices: [
+        "Ephesus",
+        "Galatia",
+        "Philippi",
+        "Colossae"
+      ],
+      answer: 2,
+      reference: "1 Thessalonians 2:2",
+      link: "https://www.blueletterbible.org/kjv/1th/2/2"
+    },
+    {
+      question_id: 4,
+      question: "Israelites were told to make an offer of peace to a city they were about to attack. What happened if the city accepted the offer?",
+      choices: [
+        "God would bless the people of the city",
+        "they would be taken back to Jerusalem as servants",
+        "everyone in that city was killed",
+        "everyone in that city became their slaves"
+      ],
+      answer: 3,
+      reference: "Deuteronomy 20:10-11",
+      link: "https://www.blueletterbible.org/kjv/deu/20/10"
+    },
+    {
+      question_id: 5,
+      question: "What does Paul mean when he says, 'the traditions of my fathers'?",
+      choices: [
+        "the Torah",
+        "the rabbinic oral law",
+        "Gentile customs",
+        "the teachings of Jesus"
+      ],
+      answer: 1,
+      reference: "Galatians 1:13-14",
+      link: "https://www.blueletterbible.org/kjv/gal/1/13"
+    },
+    {
+      question_id: 6,
+      question: "What is a farmer who won't plow because of the cold called in Proverbs?",
+      choices: [
+        "a sluggard",
+        "a wise man",
+        "a fool",
+        "a mocker"
+      ],
+      answer: 0,
+      reference: "Proverbs 20:4",
+      link: "https://www.blueletterbible.org/kjv/pro/20/4"
+    },
+    {
+      question_id: 7,
+      question: "To what does James compare a man who doubts?",
+      choices: [
+        "shifting shadows",
+        "waves of the sea",
+        "leaves blowing in the wind",
+        "an uprooted tree"
+      ],
+      answer: 1,
+      reference: "James 1:6",
+      link: "https://www.blueletterbible.org/kjv/jas/1/6"
+    },
+  ]
+};
