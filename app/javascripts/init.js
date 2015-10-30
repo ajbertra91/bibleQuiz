fb0a3d695872e047e9148501c44c9997b060c39e
Adam <adam@sportstechinc.com>
2015-10-30 11:18:36 -0400

    adds next button

--- app/javascripts/init.js
+++ app/javascripts/init.js
@@ -45,71 +45,6 @@ document.addEventListener("DOMContentLoaded", function() {
   //   'labeled-slider': labelSlider 
   // })
 
-  //BIBLE QUIZ
-  function renderChoices(choices, choice) {
-    console.debug('choice: ', choice);
-    console.debug('choices: ', choices);
-    return h('ul.list-group',[
-      h(choice.choice === 'one' ? 'li.list-group-item.choice.one.is-selected' : 'li.list-group-item.choice.one', choices[0]),
-      h(choice.choice === 'two' ? 'li.list-group-item.choice.two.is-selected' : 'li.list-group-item.choice.two', choices[1]),
-      h(choice.choice === 'three' ? 'li.list-group-item.choice.three.is-selected' : 'li.list-group-item.choice.three', choices[2]),
-      h(choice.choice === 'four' ? 'li.list-group-item.choice.four.is-selected' : 'li.list-group-item.choice.four', choices[3])
-    ]);
-  }
-
-  function view(state$) {
-    let qObj = game.questions;
-    let total = game.questions.length;
-    return state$.map((choice) => 
-      h('div.quiz-container.jumbotron', [
-        h('div.question-container.container',[
-          h('h2.text', qObj[choice.turn].question),
-          h('div.score-container.container', [
-            h('h3.completed-questions.page-header', [
-              h('span.completed.pull-left.', `${choice.turn+1}`),
-              h('span.total.pull-left', '/'+`${total}`),
-              h('span.percent-corrent.pull-right', '0%')
-            ]),
-          ])
-        ]),
-        h('div.choices-container.container', [
-          renderChoices(qObj[choice.turn].choices, choice),
-          h('div#submit.btn.btn-default', 'Submit'),
-        ]),
-        h('div.answer-container.container', [
-          h('h3.verse-container.page-header', [
-            // TODO this needs to be hidden until the user clicks the SUBMIT button
-            // and the answer is evalutated correct or incorrect
-            h('a.ref-link', {href: qObj[choice.turn].link, target: '_blank'}, [ qObj[choice.turn].reference ]),
-          ])
-        ])
-      ])
-    )
-  }
-
-  function model(actions) {
-    let turn = 0;
-    return Cycle.Rx.Observable.combineLatest(
-      actions.choice$.startWith(''),
-      actions.submitClick$.startWith(false),
-      // actions.turn$.startWith(0).scan((a, b) => a + b).map(log),
-      (choice, submit) => ({ choice: ((submit === true) ? '' : choice), submit, turn: ((submit === true) ? (turn = turn + 1) : turn) })
-    );
-  }
-
-  function intent(DOM) {
-    console.debug('DOM: ', DOM);
-    return {
-      choice$: DOM.select('.choice').events('click').map(ev => ev.target.classList[2]),
-      submitClick$: DOM.select('#submit').events('click').map(ev => true)
-      // turn$: DOM.select('#submit').events('click').map(ev => +1)
-    };
-  }
-
-  function main({DOM}) {
-    return { DOM: view(model(intent(DOM))) }
-  }
-
   // BMI CALCULATOR
   // function bmiStatus(bmi) {
   //   if (bmi < 18.5) {
@@ -224,6 +159,101 @@ document.addEventListener("DOMContentLoaded", function() {
   //       )
   //   };
   // }
+
+  // QUIZ
+  function renderChoices(choices, choice) {
+    console.debug('choice: ', choice);
+    console.debug('choices: ', choices);
+    let choiceElements;
+    if (choice.choice.length > 0 && choice.submit === false) {
+      choiceElements = h('div', [
+        h('ul.list-group',[
+          h(choice.choice === 'one' ? 'li.list-group-item.choice.one.is-selected' : 'li.list-group-item.choice.one', choices[0]),
+          h(choice.choice === 'two' ? 'li.list-group-item.choice.two.is-selected' : 'li.list-group-item.choice.two', choices[1]),
+          h(choice.choice === 'three' ? 'li.list-group-item.choice.three.is-selected' : 'li.list-group-item.choice.three', choices[2]),
+          h(choice.choice === 'four' ? 'li.list-group-item.choice.four.is-selected' : 'li.list-group-item.choice.four', choices[3])
+        ]),
+        h('div#submit.btn.btn-primary.pull-left', 'Sumbit')
+      ]);
+    } else if (choice.choice.length > 0 && choice.submit === true) {
+      choiceElements = h('div', [
+        h('ul.list-group',[
+          h(choice.choice === 'one' ? 'li.list-group-item.choice.one.is-selected' : 'li.list-group-item.choice.one', choices[0]),
+          h(choice.choice === 'two' ? 'li.list-group-item.choice.two.is-selected' : 'li.list-group-item.choice.two', choices[1]),
+          h(choice.choice === 'three' ? 'li.list-group-item.choice.three.is-selected' : 'li.list-group-item.choice.three', choices[2]),
+          h(choice.choice === 'four' ? 'li.list-group-item.choice.four.is-selected' : 'li.list-group-item.choice.four', choices[3])
+        ]),
+        h('div#next.btn.btn-primary.pull-right', 'Next')
+      ]);
+    } else {
+      choiceElements = h('div', [
+        h('ul.list-group',[
+          h(choice.choice === 'one' ? 'li.list-group-item.choice.one.is-selected' : 'li.list-group-item.choice.one', choices[0]),
+          h(choice.choice === 'two' ? 'li.list-group-item.choice.two.is-selected' : 'li.list-group-item.choice.two', choices[1]),
+          h(choice.choice === 'three' ? 'li.list-group-item.choice.three.is-selected' : 'li.list-group-item.choice.three', choices[2]),
+          h(choice.choice === 'four' ? 'li.list-group-item.choice.four.is-selected' : 'li.list-group-item.choice.four', choices[3])
+        ])
+      ]);
+    }
+    return choiceElements;
+  }
+
+  function view(state$) {
+    let qObj = game.questions;
+    let total = game.questions.length;
+    return state$.map((choice) => 
+      h('div.quiz-container.jumbotron', [
+        h('div.question-container.container',[
+          h('h2.text', qObj[choice.turn].question),
+          h('div.score-container.container', [
+            h('h3.completed-questions.page-header', [
+              h('span.completed.pull-left.', `${choice.turn+1}`),
+              h('span.total.pull-left', '/'+`${total}`),
+              h('span.percent-corrent.pull-right', '0%')
+            ]),
+          ])
+        ]),
+        h('div.choices-container.container', [
+          renderChoices(qObj[choice.turn].choices, choice)
+        ]),
+        h('div.answer-container.container', [
+          h('h3.verse-container.page-header', [
+            // TODO this needs to be hidden until the user clicks the SUBMIT button
+            // and the answer is evalutated correct or incorrect
+            h('a.ref-link', {href: qObj[choice.turn].link, target: '_blank'}, [ qObj[choice.turn].reference ]),
+          ])
+        ])
+      ])
+    )
+  }
+
+  function model(actions) {
+    let turn = 0;
+    return Cycle.Rx.Observable.combineLatest(
+      actions.choice$.startWith(''),
+      actions.submitClick$.startWith(false),
+      actions.nextClick$.startWith(false),
+      // actions.turn$.startWith(0).scan((a, b) => a + b).map(log),
+      (choice, submit, next) => ({ choice: ((next === true) ? '' : choice),
+                                   submit: ((next === true) ? false : submit),
+                                   next,
+                                   turn: ((next === true) ? (turn = turn + 1) : turn) })
+    );
+  }
+
+  function intent(DOM) {
+    console.debug('DOM: ', DOM);
+    return {
+      choice$: DOM.select('.choice').events('click').map(ev => ev.target.classList[2]),
+      submitClick$: DOM.select('#submit').events('click').map(ev => true),
+      nextClick$: DOM.select('#next').events('click').map(ev => true)
+      // turn$: DOM.select('#submit').events('click').map(ev => +1)
+    };
+  }
+
+  function main({DOM}) {
+    return { DOM: view(model(intent(DOM))) }
+  }
 
   let drivers = {
     DOM: makeDOMDriver('#app')
